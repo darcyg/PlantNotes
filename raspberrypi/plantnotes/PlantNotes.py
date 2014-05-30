400d2271bff0d3d9f1a344da8a3632283ec82f5b
jakemalley <jja.malley@gmail.com>
2014-05-30 18:48:54 +0100

    Added KeyboardInterrupt exception.

--- raspberrypi/plantnotes/PlantNotes.py
+++ raspberrypi/plantnotes/PlantNotes.py
@@ -8,31 +8,44 @@
 import SocketServer, RF24Listener, UpdateMySQL # Our Files
 import sys, time
 
-p = int(raw_input("Enter port> "))
-server = SocketServer.SocketThread(1, "SocketThread", p)
-server.start()
-
+port = int(raw_input("Enter port> ")) # For testing only, actual service will run on port 5021
+server = SocketServer.SocketThread(1, "SocketThread", port)
 radio_listener = RF24Listener.RF24ListenerThread(2, "ListenThread")
+
+# Start both threads.
+server.start()
 radio_listener.start()
 
 while True:
 	
-	# Deal with socket messages
-	if SocketServer.message_queue:
-		# Get + remove message
-		socket_message = SocketServer.message_queue[0]
-		SocketServer.message_queue.remove(socket_message)
-		# Add the message to thr writing queue
-		RF24Listener.writing_queue.append(socket_message)
-		
-	# Deal with RF24 Messages
-	if RF24Listener.message_queue:
-		# Get + remove message
-		rf24_message = RF24Listener.message_queue[0]
-		RF24Listener.message_queue.remove(rf24_message)
-		# Make a new thread to input this to SQL
-		sql_thread = UpdateMySQL.InputData(3, "SQLThread",rf24_message)
-		sql_thread.start()
+	try:
+		# Deal with socket messages
+		if SocketServer.message_queue:
+			# Get + remove message
+			socket_message = SocketServer.message_queue[0]
+			SocketServer.message_queue.remove(socket_message)
+			# Add the message to thr writing queue
+			RF24Listener.writing_queue.append(socket_message)
+			
+		# Deal with RF24 Messages
+		if RF24Listener.message_queue:
+			# Get + remove message
+			rf24_message = RF24Listener.message_queue[0]
+			RF24Listener.message_queue.remove(rf24_message)
+			# Make a new thread to input this to SQL
+			sql_thread = UpdateMySQL.InputData(3, "SQLThread",rf24_message)
+			sql_thread.start()
+
+	except KeyboardInterrupt:
+
+		# Close the connection and socket.
+		server.conn.close()
+		server.sock.close()
+
+		print("Exiting")
+		# Break the loop.
+		break
+
 
 	time.sleep(0.5)
 
